e9b80d47ff94586c443d224d6c545e2056b63365
lucasn4 <168351521+lucasn4@users.noreply.github.com>
2025-01-23 01:47:32 -0300

    commit corregido minusculas

--- src/assets/imagenes/imagen.js
+++ src/assets/imagenes/imagen.js
@@ -5,12 +5,12 @@ import Actualizarhisclima from './actualizarhisclima.png';
 import Pagprincipalhostal from './pagprincipalhostal.png';
 import Fechahabitaciones from './fechahabitaciones.png';
 import Menuadminactual from './menuadminactual.png';
-import Menuhabitacionesocupadas from './Menuhabitacionesocupadas.png';
+import Menuhabitacionesocupadas from './menuhabitacionesocupadas.png';
 import PerfilProf from './fotoperfilprofesional.jpg';
 import Comandovozprincipal from './comandovozprincipal.png';
-import Comandovozinvitados from './Comandovozinvitados.png';
+import Comandovozinvitados from './comandovozinvitados.png';
 import Comandovozlugares from './comandovozlugares.png';
-import Comandovoztareas from './Comandovoztareas.png';
+import Comandovoztareas from './comandovoztareas.png';
 export { Pythonprecarga, Pythoncargado, Imagenbusclima, Actualizarhisclima, Pagprincipalhostal, Fechahabitaciones, Menuadminactual, Menuhabitacionesocupadas, PerfilProf,
     Comandovozprincipal, Comandovozinvitados, Comandovozlugares, Comandovoztareas
  };
